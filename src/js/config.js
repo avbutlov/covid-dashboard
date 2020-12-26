9b58bcbdab3983703d7900466add472602ef2ea9
avbutlov <63131481+avbutlov@users.noreply.github.com>
2020-12-27 01:19:33 +0300

    fix: map key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://disease.sh/v3/covid-19';
-export const MAP_API_URL = 'https://api.maptiler.com/maps/topo/{z}/{x}/{y}.png?key=CWiuDOeHgf5lER97hFr0';
+export const MAP_API_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const COUNT_PEOPLE = 7858772994;
